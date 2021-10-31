c13e8246abcf4d7de26ae4d351f8f8b60a7c6085
Allen DeHoff <allen.dehoff21@gmail.com>
2021-10-31 10:30:28 -0600

    changed to res_x and chagned range

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ async function getSheetsClient() {
      console.log('made sheets');
      sheets.spreadsheets.values.get({
          spreadsheetId: '1H_Z3GNWuOIb4xtjhToD7OVE3zcZDNDTQ3muX08E4SpU',
-         range: 'Sheet1!A:C',
+         range: 'Sheet1!A:D',
      }, (err, res) => {
          console.log('callback entry');
          if (err) {
             console.log('The API returned an error: ' + err);
-            res.status(500).send(err);
+            res_x.status(500).send(err);
          }
         const rows = res.data.values;
         console.log("no error");
         if (rows.length) {
             console.log("got rows");
-            res.status(200).send(JSON.stringify(rows));
+            res_x.status(200).send(JSON.stringify(rows));
         } else {
             console.log("no data found");
-            res.status(404).send('No data found.');
+            res_x.status(404).send('No data found.');
         }
      })
   };
