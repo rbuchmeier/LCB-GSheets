7260e3b70f1ae537a4f4649df3ea7b3e4c14b73c
Ryan Buchmeier <rwbleo@gmail.com>
2021-10-31 10:57:03 -0600

    Works for upload

--- index.js
+++ index.js
@@ -12,24 +12,29 @@ async function getSheetsClient() {
  exports.main = async (req_x, res_x) => {
      const sheets = await getSheetsClient();
      console.log('made sheets');
-     sheets.spreadsheets.values.get({
-         spreadsheetId: '1H_Z3GNWuOIb4xtjhToD7OVE3zcZDNDTQ3muX08E4SpU',
-         range: 'Sheet1!A:D',
-     }, (err, res) => {
-         console.log('callback entry');
-         if (err) {
-            console.log('The API returned an error: ' + err);
-            res_x.status(500).send(err);
-         }
-        const rows = res.data.values;
-        console.log("no error");
-        if (rows.length) {
-            console.log("got rows");
-            res_x.status(200).send(JSON.stringify(rows));
-        } else {
-            console.log("no data found");
-            res_x.status(404).send('No data found.');
+     sheets.spreadsheets.values.append({
+        spreadsheetId: '1H_Z3GNWuOIb4xtjhToD7OVE3zcZDNDTQ3muX08E4SpU',
+        range: 'Sheet1!A:D',
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+            values: [
+                [
+                    new Date().toLocaleDateString("en-US"),
+                    req_x.body.user,
+                    req_x.body.comment,
+                    req_x.body.filename,
+                ]
+            ]
         }
+     }, (err, res) => {
+        console.log('callback entry');
+        if (err) {
+           console.log('The API returned an error: ' + err);
+           res_x.status(500).send(err);
+        }
+        console.log("We got a response");
+        console.log(JSON.stringify(res));
+        res_x.status(200).send();
      })
   };
   
